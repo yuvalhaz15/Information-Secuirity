5837ec71b919ab8fef5399e887a10366709a29b6
yuvalhaz15 <yuvalhaz@post.bgu.ac.il>
2022-11-19 16:00:10 +0200

    finish encrypt

--- src/CipherBlockChaining.java
+++ src/CipherBlockChaining.java
@@ -3,12 +3,12 @@
 import java.io.*;
 import java.util.*;
 public class CipherBlockChaining {
-
+   private static HashMap<Character, Character> keyDictonary = new HashMap<Character, Character>();
+   private static String chiperText="";
     private static String readString(String file) {//convert file to a long String
         String text = "";
         try {
             Scanner s = new Scanner (new File(file));
-            s.nextLine();
             while (s.hasNextLine()) {
                 text+=s.nextLine()+ "\t";
             }
@@ -18,36 +18,46 @@ public class CipherBlockChaining {
         }
         return text;
     }
-    public static  String[] createFile (String file) {
+    private static  String[] createFile (String file) {
         String text = readString(file);
         return text.split("\t");
     }
-
-    private static void initMapDictanory(String key, HashMap<Character, Character> keyDictonary) {
+    private static void initWithAllEnglishLetters() {
         for (char ch = 'a'; ch <= 'z'; ++ch)
             keyDictonary.put(ch, ch);
         for (char ch = 'A'; ch <= 'Z'; ++ch)
             keyDictonary.put(ch, ch);
+    }
 
+    private static void initMapDictanory(String key) {
+        initWithAllEnglishLetters();
+        for (int i = 0; i < key.length(); i += 4)
+            keyDictonary.replace(key.charAt(i), key.charAt(i+2));
 
 
     }
-    private static void makeXorBetweenPlainTextAndVectorIv(String chiperText, String plain_text, String init_vector) {
-     for (int i=0;i<plain_text.length();i++){
+
+
+
+    private static void makeXorBetweenPlainTextAndVectorIv( String plain_text, String init_vector) {
+     for (int i=0;i<plain_text.length();i++)
      chiperText+= (char) ((int) plain_text.charAt(i) ^ (int) init_vector.charAt(i));
 
-     }
+
 
     }
-    private static void useKeyToCreateFinalCipherText(String chiperText, String init_vector, String key) {
+    private static void useKeyToCreateFinalCipherText() {
+        String replacedChiperText="";
+        for (int i=0;i<chiperText.length();i++)
+            replacedChiperText+=keyDictonary.get(chiperText.charAt(i));
+        chiperText=replacedChiperText;
+
     }
     public static String encrypt(String plain_text, String init_vector, String key){
-        String chiperText="";
-        HashMap<Character, Character> keyDictonary = new HashMap<Character, Character>();
 
-        initMapDictanory(key,keyDictonary);
-        makeXorBetweenPlainTextAndVectorIv(chiperText,plain_text,init_vector);
-        useKeyToCreateFinalCipherText(chiperText,init_vector,key);
+        initMapDictanory(key);
+        makeXorBetweenPlainTextAndVectorIv(plain_text,init_vector);
+        useKeyToCreateFinalCipherText();
 
         return chiperText ;
     }
--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,18 @@ public class Main {
     private static String readString(String file) {//convert file to a long String
         String text = "";
         try {
-            Scanner s = new Scanner (new File(file));
-            s.nextLine();
+            Scanner s = new Scanner(new File(file));
+
             while (s.hasNextLine()) {
-                text+=s.nextLine()+ "\t";
+                text += s.nextLine() + "\t";
             }
-        }
-        catch (FileNotFoundException e) {//Throw an exception if the file is not found
-            System.out.println("file "+ file + " not found");
+        } catch (FileNotFoundException e) {//Throw an exception if the file is not found
+            System.out.println("file " + file + " not found");
         }
         return text;
     }
-    public static  String[] createFile (String file) {
+
+    public static String[] createFile(String file) {
         String text = readString(file);
         return text.split("\t");
     }
@@ -24,8 +24,24 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        String check="a-b\r\nb-c\r\nc-c";
-        System.out.println(readString("src/myKey.txt"));
-        System.out.println(check);
+        String check = "a-b\r\nb-c\r\nc-c";
+        String fileString = readString("src/myKey.txt");
+//        System.out.println(fileString);
+
+        String checkChiper = CipherBlockChaining.encrypt("ABCDQRSTA", "0000000000", fileString);
+        System.out.println(checkChiper);
+//
+//        HashMap<Character, Character> keyDictonary = new HashMap<Character, Character>();
+//        for (char ch = 'a'; ch <= 'z'; ++ch)
+//            keyDictonary.put(ch, ch);
+//        for (char ch = 'A'; ch <= 'Z'; ++ch)
+//            keyDictonary.put(ch, ch);
+//
+//
+//        for (Map.Entry<Character, Character> set : keyDictonary.entrySet())
+//            System.out.println(set.getKey() + " - " + set.getValue());
+
+
     }
+
 }
